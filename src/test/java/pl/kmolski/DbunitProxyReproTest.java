497e3e0a559313da99e699943bf5afe9263b92a2
kmolski <krzysztof.molski29@gmail.com>
2022-08-05 14:54:07 +0200

    First commit

---
+++ src/test/java/pl/kmolski/DbunitProxyReproTest.java
@@ -0,0 +1,43 @@
+package pl.kmolski;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.dbunit.DataSourceBasedDBTestCase;
+import org.dbunit.JdbcBasedDBTestCase;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.statement.StatementFactory;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.postgresql.PostgresqlDataTypeFactory;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+import static org.junit.Assert.assertThrows;
+
+public class DbunitProxyReproTest extends DataSourceBasedDBTestCase {
+
+    @Override
+    protected DataSource getDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:tc:postgresql:14.2:///test?TC_INITSCRIPT=init.sql");
+        return new HikariDataSource(config);
+    }
+
+    @Override
+    protected void setUpDatabaseConfig(DatabaseConfig config) {
+        super.setUpDatabaseConfig(config);
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new PostgresqlDataTypeFactory());
+    }
+
+    @Override
+    protected IDataSet getDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(getClass().getClassLoader().getResourceAsStream("data.xml"));
+    }
+
+    public void testWithHikariDataSource() throws Exception {
+        IDataSet dataset = getConnection().createDataSet();
+        assertEquals(1, dataset.getTable("foo").getRowCount());
+    }
+}
---
+++ src/test/resources/init.sql
@@ -0,0 +1,1 @@
+create table if not exists foo (id serial, baz oid);
